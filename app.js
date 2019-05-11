b6603f0cc817997709f5527a04f5d484716bf62d
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-11 22:58:40 +0530

    Data binding

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Sulay",
-    job: "Artist"
+    job: "Artist",
+    website: "https://sulaysumaria.github.io",
+    websiteTag:
+      '<a href="https://sulaysumaria.github.io" target="_blank">Sulay Sumaria</a>'
   },
   methods: {
     greet(time) {
