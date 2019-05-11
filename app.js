5bf108f43fe2476926ef3fcd001a115c16419cef
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-11 23:14:07 +0530

    Events

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ new Vue({
     job: "Artist",
     website: "https://sulaysumaria.github.io",
     websiteTag:
-      '<a href="https://sulaysumaria.github.io" target="_blank">Sulay Sumaria</a>'
+      '<a href="https://sulaysumaria.github.io" target="_blank">Sulay Sumaria</a>',
+    age: 23,
+    x: 0,
+    y: 0
   },
   methods: {
     greet(time) {
       return `Good ${time}, ${this.name}`;
+    },
+    addYear(years) {
+      this.age += years;
+    },
+    subtractYear(years) {
+      this.age -= years;
+    },
+    updateXY(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
