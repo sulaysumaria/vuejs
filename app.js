df06e1dc0477ba59888dcea0ef391e9f4f2f8e81
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-12 13:43:18 +0530

    Simple punchbag game.

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Sulay",
-    job: "Artist",
-    website: "https://sulaysumaria.github.io",
-    websiteTag:
-      '<a href="https://sulaysumaria.github.io" target="_blank">Sulay Sumaria</a>',
-    age: 23,
-    x: 0,
-    y: 0,
-    a: 0,
-    b: 0,
-    available: false,
-    nearby: false,
-    error: false,
-    success: false,
-    characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
-    ninjas: [
-      { name: "Ryu", age: 25 },
-      { name: "Yoshi", age: 35 },
-      { name: "Ken", age: 55 }
-    ]
+    health: 100,
+    ended: false
   },
   methods: {
-    greet(time) {
-      return `Good ${time}, ${this.name}`;
+    punch() {
+      this.health -= 10;
+
+      if (this.health <= 0) {
+        this.ended = true;
+      }
     },
-    addYear(years) {
-      this.age += years;
-    },
-    subtractYear(years) {
-      this.age -= years;
-    },
-    updateXY(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click() {
-      alert("You clicked me");
-    },
-    logName() {
-      console.log("You have entered name.");
-    },
-    logAge() {
-      console.log("You have entered age.");
+    restart() {
+      this.health = 100;
+      this.ended = false;
     }
   },
-  computed: {
-    addToA() {
-      return this.age + this.a;
-    },
-    addToB() {
-      return this.age + this.b;
-    },
-    compClasses() {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      };
-    }
-  }
+  computed: {}
 });
