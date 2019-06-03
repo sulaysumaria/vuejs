0b773ed61d47c7d4614cdf01cd88aa5bf9320414
Sulay Sumaria <sulaysumaria@gmail.com>
2019-06-03 16:06:19 +0530

    Nesting components.

---
+++ src/names.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li :key="name" v-for="name in data">{{name}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "names",
+  data() {
+    return {
+      data: ["Mario", "Luigi", "Princess"]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
